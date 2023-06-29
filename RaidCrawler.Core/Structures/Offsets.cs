85e2dffedb172140ab87f109b10db89fbbb41733
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2023-06-29 23:04:26 +1000

    Casing consistency

--- RaidCrawler.Core/Structures/Offsets.cs
+++ RaidCrawler.Core/Structures/Offsets.cs
@@ -7,7 +7,7 @@
         
         public IReadOnlyList<long> SaveBlockPointer = new List<long>() { 0x44AAC88, 0xE0, 0x80, 0x08, 0x0 }; // Thanks Lincoln-LM!
         public IReadOnlyList<long> RaidBlockPointer = new List<long>() { 0x44C1C18, 0x180, 0x40 };
-        public IReadOnlyList<long> BlockKeyPointer = new List<long>() { 0x44b71A8, 0xD8, 0x0, 0x0, 0x30, 0x0 };
+        public IReadOnlyList<long> BlockKeyPointer = new List<long>() { 0x44B71A8, 0xD8, 0x0, 0x0, 0x30, 0x0 };
         public IReadOnlyList<uint> DifficultyFlags = new List<uint>() { 0xEC95D8EF, 0xA9428DFE, 0x9535F471, 0x6E7F8220 };
 
         public const uint BCATRaidBinaryLocation = 0x520A1B0; // Thanks Lincoln-LM!
