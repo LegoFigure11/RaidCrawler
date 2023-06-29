571e20fa7124c6c523d47622dbce1f627e8be9ba
santacrab2 <79347566+santacrab2@users.noreply.github.com>
2023-06-29 09:05:00 -0400

    SV v1.3.2 Pointer Updates (#154)
    * Updates RaidBlockPointer (Thank you Manu)
    Updates BlockKeyPointer
    
    * Casing consistency
    
    ---------
    
    Co-authored-by: LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>

--- RaidCrawler.Core/Structures/Offsets.cs
+++ RaidCrawler.Core/Structures/Offsets.cs
@@ -4,10 +4,10 @@
     {
         public const string ScarletID = "0100A3D008C5C000";
         public const string VioletID = "01008F6008C5E000";
-
+        
         public IReadOnlyList<long> SaveBlockPointer = new List<long>() { 0x44AAC88, 0xE0, 0x80, 0x08, 0x0 }; // Thanks Lincoln-LM!
-        public IReadOnlyList<long> RaidBlockPointer = new List<long>() { 0x44BFBA8, 0x180, 0x40 };
-        public IReadOnlyList<long> BlockKeyPointer = new List<long>() { 0x44B5158, 0xD8, 0x0, 0x0, 0x30, 0x0 };
+        public IReadOnlyList<long> RaidBlockPointer = new List<long>() { 0x44C1C18, 0x180, 0x40 };
+        public IReadOnlyList<long> BlockKeyPointer = new List<long>() { 0x44B71A8, 0xD8, 0x0, 0x0, 0x30, 0x0 };
         public IReadOnlyList<uint> DifficultyFlags = new List<uint>() { 0xEC95D8EF, 0xA9428DFE, 0x9535F471, 0x6E7F8220 };
 
         public const uint BCATRaidBinaryLocation = 0x520A1B0; // Thanks Lincoln-LM!
