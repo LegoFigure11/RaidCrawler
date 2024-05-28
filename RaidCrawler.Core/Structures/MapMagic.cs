02649788c2bac53290532e2a04cf7d0aaeb88c38
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2024-05-28 19:30:31 +1000

    MapMagic: Mark fields as private

--- RaidCrawler.Core/Structures/MapMagic.cs
+++ RaidCrawler.Core/Structures/MapMagic.cs
@@ -4,9 +4,9 @@ namespace RaidCrawler.Core.Structures
 {
     public class MapMagic
     {
-        public double ScaleX { get; init; }
-        public double ScaleZ { get; init; }
-        public double OffsetZ { get; init; }
+        private double ScaleX { get; init; }
+        private double ScaleZ { get; init; }
+        private double OffsetZ { get; init; }
         public double ConvertWidth(double s) => (512 / ScaleX) * s;
         public double ConvertHeight(double s) => (512 / ScaleZ) * s;
         public double ConvertX(double x) => (512 / ScaleX) * x;
